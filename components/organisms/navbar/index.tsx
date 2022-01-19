6372c3953ca5796c7989359a477472c433268397
Fahmi Idris <fahmiidris.1607@gmail.com>
2022-01-19 10:36:24 +0700

    fix (posts): delete posts components

--- components/organisms/navbar/index.tsx
+++ components/organisms/navbar/index.tsx
@@ -42,7 +42,7 @@ export const Navbar = () => {
 
                                 <div className="basis-1/3">
                                     <div className="flex items-center justify-end space-x-4">
-                                        <button type="button">
+                                        <Link href="/">
                                             <svg
                                                 aria-label="Home"
                                                 color="#262626"
@@ -60,8 +60,8 @@ export const Navbar = () => {
                                                     strokeWidth={2}
                                                 />
                                             </svg>
-                                        </button>
-                                        <button type="button">
+                                        </Link>
+                                        <Link href="#">
                                             <svg
                                                 aria-label="Messenger"
                                                 color="#262626"
@@ -83,8 +83,8 @@ export const Navbar = () => {
                                                     fillRule="evenodd"
                                                 />
                                             </svg>
-                                        </button>
-                                        <button type="button">
+                                        </Link>
+                                        <Link href="#">
                                             <svg
                                                 aria-label="New Post"
                                                 color="#262626"
@@ -125,8 +125,8 @@ export const Navbar = () => {
                                                     y2="17.455"
                                                 />
                                             </svg>
-                                        </button>
-                                        <button type="button">
+                                        </Link>
+                                        <Link href="#">
                                             <svg
                                                 aria-label="Find People"
                                                 color="#262626"
@@ -159,8 +159,8 @@ export const Navbar = () => {
                                                     strokeWidth={2}
                                                 />
                                             </svg>
-                                        </button>
-                                        <button type="button">
+                                        </Link>
+                                        <Link href="#">
                                             <svg
                                                 aria-label="Activity Feed"
                                                 color="#262626"
@@ -172,7 +172,7 @@ export const Navbar = () => {
                                             >
                                                 <path d="M16.792 3.904A4.989 4.989 0 0121.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 014.708-5.218 4.21 4.21 0 013.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 013.679-1.938m0-2a6.04 6.04 0 00-4.797 2.127 6.052 6.052 0 00-4.787-2.127A6.985 6.985 0 00.5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 003.518 3.018 2 2 0 002.174 0 45.263 45.263 0 003.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 00-6.708-7.218z" />
                                             </svg>
-                                        </button>
+                                        </Link>
                                         <button
                                             type="button"
                                             className="relative aspect-square h-[25px] rounded-full overflow-hidden"
